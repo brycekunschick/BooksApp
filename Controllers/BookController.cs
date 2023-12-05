46b5b7f28875cc33c8392b1513bcaf6d3bcd3126
brycekunschick <150211483+brycekunschick@users.noreply.github.com>
2023-12-05 13:31:15 -0500

    11/9
    -	Adding a view for the Create action method
    o	Right click on the word “Create” in BookController.cs
    o	Add an empty razor view
    -	At 17:26
    -	Views Folder -> shared -> layout
    o	Contains the items that appear on every screen
    -	Updating the database – 2 parts
    o	1: The view – the empty form or prompt
    o	2: The post – you are ready to post the book object you created

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -24,9 +24,22 @@ namespace BooksApp_Sec02.Controllers
             return View(booksList);
         }
 
+        [HttpGet]
+        //this is an http get request; you are getting the form
         public IActionResult Create() 
         {
             return View();
+        }
+
+        //this is an http post request, you are posting the data
+        [HttpPost]
+        public IActionResult Create(Book bookObj)
+        {
+            _context.books.Add(bookObj);//getting ready to add the book
+            _context.SaveChanges();//this is when we actually add the book to the db
+
+            return RedirectToAction("Index", "Book");
+
         }
 
     }

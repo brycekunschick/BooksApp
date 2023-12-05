f23ebd2793eff8bd8c418dbd333ecf5d34a48f13
brycekunschick <150211483+brycekunschick@users.noreply.github.com>
2023-12-04 22:31:56 -0500

    10/26 - (check commit description)
    Project Created +
    Title changed to Books Application
    books class made in the models folder
    public stuff added to books class

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BooksApp_Sec02.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime DatePublished { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksApp_Sec02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
